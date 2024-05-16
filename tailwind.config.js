9c01ff590f36287d7bcec97d1316515777536228
Miles <juniornyamile@gmail.com>
2024-05-16 14:13:22 +0000

    iniial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  future: {},
+  purge: [],
+  variants: {},
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
